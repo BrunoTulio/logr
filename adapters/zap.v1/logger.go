1242d53504a368ec0c04a6ac7cdb3bae5cab1360
Bruno Túlio <brunotulio.engcomp@gmail.com>
2025-11-30 18:43:56 -0300

    Revert "Merge pull request #12 from BrunoTulio/fix/logger-duplicate-fields"
    This reverts commit 5b6d6fa485f2bd369a34409dd3f23bacac842df0, reversing
    changes made to c36eaab857d0ab21dcbce4675bb318962dc44177.

--- adapters/zap.v1/logger.go
+++ adapters/zap.v1/logger.go
@@ -109,7 +109,7 @@ func (l *logger) WithField(field logr.Field) logr.Logger {
 func (l *logger) WithFields(fields ...logr.Field) logr.Logger {
 	//nolint:gocritic // appendAssign: necessário criar nova slice para manter imutabilidade
 	newFields := append(l.fields, fields...)
-	args := buildSugaredArgs(fields)
+	args := buildSugaredArgs(newFields)
 
 	return &logger{
 		option: l.option,
--- adapters/zerolog.v1/logger.go
+++ adapters/zerolog.v1/logger.go
@@ -109,7 +109,7 @@ func (l *logger) WithField(field logr.Field) logr.Logger {
 func (l *logger) WithFields(fields ...logr.Field) logr.Logger {
 	//nolint:gocritic // appendAssign: necessário criar nova slice para manter imutabilidade
 	newFields := append(l.fields, fields...)
-	args := buildAttrs(fields)
+	args := buildAttrs(newFields)
 	newLogger := l.logger.With().Fields(args).Logger()
 
 	return &logger{
